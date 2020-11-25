7936cb30d17a9c1b7814eedbc7d6ac849e2f004d
MarcosMurillo <murillo.psoft@gmail.com>
2020-11-25 18:30:46 -0300

    dealing with responsive supplier compenent

--- frontend/src/components/Supplier/index.js
+++ frontend/src/components/Supplier/index.js
@@ -1,56 +1,60 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { FaUser, FaEye, FaPhone, FaFax, FaReply } from 'react-icons/fa';
+import React from "react";
+import PropTypes from "prop-types";
+import { FaUser, FaEye, FaPhone, FaFax, FaReply } from "react-icons/fa";
 
 export default function Supplier({ supplierInfo }) {
+  //responsivo alterado
   return (
-    <div className='e-box --transition-off'>
-      <div className='col-12 col-sm-12 col-md-12 col-lg-4 mb-3'>
-        <p className='--d-color-gray --d-text-sm'>Supplier</p>
-        <p className='--d-color-primary --d-fw-600 --d-text-md'>
-          {supplierInfo.name}
-        </p>
+    <div className="e-box --transition-off">
+      <div className="row">
+        <div className="col-12 col-md-4 col-lg-3 mb-3 mb-md-2 mb-2">
+          <p className="color-gray text-sm mb-2">Supplier</p>
+          <p className="color-primary fw-600 text-md">{supplierInfo.name}</p>
+        </div>
       </div>
 
-      <div className='row justify-content-between align-items-center'>
-        <div className='col-12 col-sm-12 col-md-12 col-lg-4'>
-          <p className='--d-text-sm'>{supplierInfo.document.value}</p>
+      <div className="row justify-content-between align-items-center">
+        <div className="col-12 col-sm-6 col-md-4 mb-md-2 mb-2">
+          <p className="text-sm">{supplierInfo.document.value}</p>
         </div>
 
-        <div className='col-12 col-sm-12 col-md-12 col-lg-4'>
-          <div className='d-flex align-items-center'>
-            <FaUser className='--d-icon-md --d-color-gray mr-1' />
-            <p className='--d-text-sm'>
-              {`${supplierInfo.contact.name}`} -{' '}
-              <span className='--d-color-primary'>
-                {supplierInfo.contact.email}
-              </span>
+        <div className="col-12 col-sm-6 col-md mb-md-2 mb-2">
+          <p className="text-sm mr-xl-2 ">
+            <FaUser className="icon-md color-gray mr-1" />
+            {`${supplierInfo.contact.name}`} -{" "}
+            <span className="color-primary">{supplierInfo.contact.email}</span>
+          </p>
+        </div>
+        <div className="col-12 col-sm-6 col-md-3 mb-md-2 mb-2">
+          <div className="dflex align-items-end">
+            <p className="text-sm">
+              <FaEye className="icon-md color-gray mr-1" />
+              {`Read: ${supplierInfo.readAt}`}
             </p>
           </div>
         </div>
-        <div className='col-12 col-sm-12 col-md-12 col-lg-4'>
-          <div className='d-flex align-items-center'>
-            <FaEye className='--d-icon-md --d-color-gray mr-1' />
-            <p className='--d-text-sm'>{supplierInfo.readAt}</p>
+
+        <div className="col-12 col-sm-6 col-md-4 mb-2">
+          <p className="text-sm text-inline-ellipsis">{supplierInfo.address}</p>
+        </div>
+        <div className="col-12 col-sm-6 col-md mb-2">
+          <div className="d-flex">
+            <p className="text-sm mr-xl-2">
+              <FaPhone className="icon-md color-gray mr-1" />
+              {`${supplierInfo.contact.phone}`}
+            </p>
+            <p className="text-sm mr-xl-2">
+              <FaFax className="icon-md color-gray mr-1" />
+              {`${supplierInfo.contact.fax}`}
+            </p>
           </div>
         </div>
-      </div>
-      <div className='row justify-content-between align-items-center'>
-        <div className='col-12 col-sm-12 col-md-12 col-lg-4'>
-          <p className='--d-text-sm'>{supplierInfo.address}</p>
-        </div>
-        <div className='col-12 col-sm-12 col-md-12 col-lg-4'>
-          <div className='d-flex align-items-center'>
-            <FaPhone className='--d-icon-md --d-color-gray mr-1' />
-            <p className='--d-text-sm'>{`${supplierInfo.contact.phone}`}</p>
-            <FaFax className='--d-icon-md --d-color-gray mr-1' />
-            <p className='--d-text-sm'>{`${supplierInfo.contact.fax}`}</p>
-          </div>
-        </div>
-        <div className='col-lg-3 col-md-6'>
-          <div className='d-flex align-items-center'>
-            <FaReply className='--d-icon-md --d-color-gray mr-1' />
-            <p className='--d-text-sm'>{supplierInfo.readAt}</p>
+        <div className="col-12 col-sm-6 col-md-3">
+          <div className="dflex align-items-end">
+            <p className="text-sm">
+              <FaReply className="icon-md color-gray mr-1" />
+              {`Last Reply: ${supplierInfo.lastReplyAt}`}
+            </p>
           </div>
         </div>
       </div>
@@ -59,7 +63,7 @@ export default function Supplier({ supplierInfo }) {
 }
 
 Supplier.defaultProps = {
-  supplierInfo: PropTypes.object
+  supplierInfo: PropTypes.object,
 };
 
 Supplier.propTypes = {
@@ -70,15 +74,15 @@ Supplier.propTypes = {
       email: PropTypes.string,
       fax: PropTypes.string,
       name: PropTypes.string,
-      phone: PropTypes.string
+      phone: PropTypes.string,
     }),
     document: PropTypes.shape({
       type: PropTypes.string,
-      value: PropTypes.string
+      value: PropTypes.string,
     }),
 
     lastReplyAt: PropTypes.string,
     name: PropTypes.string,
-    readAt: PropTypes.string
-  })
+    readAt: PropTypes.string,
+  }),
 };
--- frontend/src/pages/PurchaseOrder/index.js
+++ frontend/src/pages/PurchaseOrder/index.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Header from '../../components/Header';
-import Supplier from '../../components/Supplier';
+import React, { useState, useEffect } from "react";
+import Header from "../../components/Header";
+import Supplier from "../../components/Supplier";
 // import Card from '../../components/Card';
-import api from '../../services/api';
+import api from "../../services/api";
 
 export default function PurchaseOrder() {
   // const [addresses, setAddresses] = useState([]);
@@ -10,7 +10,7 @@ export default function PurchaseOrder() {
   const [supplier, setSupplier] = useState();
 
   async function fetchData() {
-    const response = await api.get('/orders/1');
+    const response = await api.get("/orders/1");
     return (
       // setAddresses(response.data.addresses),
       setHeader(response.data.header), setSupplier(response.data.supplier)
@@ -20,15 +20,15 @@ export default function PurchaseOrder() {
   useEffect(() => fetchData(), []);
 
   return (
-    <div className='container-fluid p-0'>
+    <>
       {header && <Header headerInfo={header} />}
-
-      <div className='row'>
-        <div className='col-12'>
-          {supplier && <Supplier supplierInfo={supplier} />}
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            {supplier && <Supplier supplierInfo={supplier} />}
+          </div>
         </div>
-      </div>
-      {/* <div className='row'>
+        {/* <div className='row'>
         <div className='col-12'>
           {addresses.map((address, index) => (
             <div key={addresses[index]} className='col-12 col-md-3 col-lg-3'>
@@ -37,6 +37,7 @@ export default function PurchaseOrder() {
           ))}
         </div>
       </div> */}
-    </div>
+      </div>
+    </>
   );
 }
