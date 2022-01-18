fc5fff5f3d2630c85f62adeb58f31224bd5453ba
Pamela Otéro <pamelasilvaoteroo@gmail.com>
2022-01-17 21:17:14 -0400

    refactor: ajuste na chamada da API
    - Chamada das informações do header
    - Renomeação das variáveis do data

--- script/index.js
+++ script/index.js
@@ -1,6 +1,19 @@
 var app = new Vue({
   el: '#header',
-  data: {
-    message: 'Desafio Mercado Eletrônico'
-  }
+
+  data () {
+    return {
+      infoHeader: null,
+      infoHeaderContact: null
+    }
+  },
+
+  mounted () {
+    axios
+      .get('https://me-frontend-challenge-api.herokuapp.com/orders/1')
+      .then(response => {
+        this.infoHeader = response.data
+      })
+      .catch(error => console.log(error))
+  },
 })
