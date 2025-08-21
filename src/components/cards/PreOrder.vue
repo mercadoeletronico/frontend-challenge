133815a20afa1acd0e3c0e76d12a248b71419cef
jefersonSantos <jefersonsaaantos@gmail.com>
2025-08-20 23:42:52 -0300

    add font-awesome for render icons

--- src/components/cards/PreOrder.vue
+++ src/components/cards/PreOrder.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex h-[110px] bg-white shadow overflow-hidden w-full">
-    <div class="bg-blue-700 rounded-r-lg text-white flex flex-col items-center justify-center px-6 py-4 min-w-[140px]">
+    <div class="bg-[#1052E0] rounded-r-lg text-white flex flex-col items-left justify-center px-6 py-4 min-w-[140px]">
       <span class="text-xs font-semibold uppercase">Pre-Order</span>
       <span class="text-2xl font-bold">{{ header.number }}</span>
       <span class="text-xs">#ME{{ header.serial }}</span>
@@ -9,25 +9,27 @@
       <div class="flex flex-col md:flex-row md:items-center md:justify-between">
         <div class="mb-2 md:mb-0">
           <div class="text-lg font-semibold text-gray-800">{{ header.buyer }}</div>
-          <div class="flex items-center text-gray-500 text-sm mt-1">
+          <div class=" items-center text-gray-500 text-sm mt-1">
             <span class="mr-2"><i class="fas fa-user"></i> {{ header?.contact?.name }}</span>
-            <span class="mr-2"><i class="fas fa-envelope"></i> {{ header?.contact?.email }}</span>
-            <span class="mr-2"><i class="fas fa-phone"></i> {{ header?.contact?.phone }}</span>
-            <span><i class="fas fa-fax"></i> {{ header?.contact?.fax }}</span>
+            <div>
+              <span class="mr-2"><i class="fas fa-envelope"></i> {{ header?.contact?.email }}</span>
+              <span class="mr-2"><i class="fas fa-phone"></i> {{ header?.contact?.phone }}</span>
+              <span><i class="fas fa-fax"></i> {{ header?.contact?.fax }}</span>
+            </div>
           </div>
         </div>
         <div class="flex flex-col items-end text-2xl">
           <div class="flex">
-            <span class="text-gray-400">{{ header.currency }}</span>
-            <span class="font-bold text-gray-700 ml-1">{{ header.price }}</span>
+            <span class="font-semibold text-[#566574]">{{ header.currency }}</span>
+            <span class="font-semibold text-[#566574] ml-1">{{ header.price }}</span>
           </div>
           <div class="flex flex-col w-full items-end">
             <div>
-              <span class="text-green-600 font-semibold text-sm">{{ header.status }}</span>
+              <span class="text-green-600 font-semibold text-xl">{{ header.status }}</span>
             </div>
             <div class="text-gray-500 text-xs">
               <span>Created at {{ formatDate(header.createdAt) }}</span>
-              <span class="ml-2 cursor-pointer"><i class="fas fa-info-circle"></i></span>
+              <span class="ml-1 cursor-pointer"><font-awesome-icon icon="fa-solid fa-circle-info" /></span>
             </div>
           </div>
         </div>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/index'
 import './style.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
+library.add(fas, far, fab)
 createApp(App)
   .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
