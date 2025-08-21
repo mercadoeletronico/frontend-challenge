bb7f2db634be92a471ae34c53d920856a5e2c6c9
Bruno Barross <altamiro_bruno@yahoo.com.br>
2025-08-21 20:06:50 -0300

    feat: add component appbadge and appcard

---
+++ src/components/ui/AppCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="rounded-lg bg-white p-4 shadow-level-1">
+    <div>
+      <p class="text-neutral-500 text-xs font-normal">{{ categoryTitle }}</p>
+      <div class="flex items-center mt-1">
+        <h3 class="text-sm text-neutral-700 font-semibold leading-[120%]">{{ title }}</h3>
+        <AppBadge v-if="showBadge" class="ml-2" text="Badge" />
+      </div>
+    </div>
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script setup>
+import AppBadge from './AppBadge.vue'
+
+const props = defineProps({
+  categoryTitle: {
+    type: String,
+    default: 'Category',
+  },
+  title: {
+    type: String,
+    default: 'Title',
+  },
+  showBadge: {
+    type: Boolean,
+    default: true,
+  },
+})
+</script>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,11 @@
 <template>
   <main>
     <h1 class="text-2xl text-primary-500">Bem vindo</h1>
-    
+    <!-- APENAS PARA VISUALIZAÇÃO DO COMPONENTE EM DESENVOLVIMENTO -->
+    <AppCard />
+    <!-- APENAS PARA VISUALIZAÇÃO DO COMPONENTE EM DESENVOLVIMENTO -->
   </main>
 </template>
+<script setup lang="ts">
+import AppCard from '../components/ui/AppCard.vue'
+</script>
