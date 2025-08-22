ba02d4d4960e438308888a2dbded6437f3cdf299
Bruno Barross <altamiro_bruno@yahoo.com.br>
2025-08-22 10:43:03 -0300

    feat: create AppSvgIcon component and add SVG sprite

---
+++ src/components/ui/AppIconSvg.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg :width="size" :height="size">
+    <use :href="iconId"></use>
+  </svg>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+    default: '16',
+  },
+})
+
+
+const iconId = computed(() => `/icons/sprite.svg#icon-${props.name}`);
+
+</script>
