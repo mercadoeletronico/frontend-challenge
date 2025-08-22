08621e831f31cb857cae3c5d7293f6d10f295f09
Bruno Barross <altamiro_bruno@yahoo.com.br>
2025-08-22 15:56:04 -0300

    feat: create AddressCard component to addresses

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -11,15 +11,30 @@
       </div>
 
     </section>
+    <section class="mt-8">
+      <div class="container mx-auto lg:px-0 px-4">
+        <AppAccordion title="Addresses">
+          <template #content>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+              <template v-for="address in orderData.value?.addresses" :key="address.id">
+                <AddressCard :supplier="orderData.value?.supplier" :address="address" />
+              </template>
 
+            </div>
+          </template>
+        </AppAccordion>
+      </div>
+    </section>
   </main>
 </template>
-<script setup lang="ts">
+<script setup>
 import { onMounted } from 'vue';
 import OrderHeader from '../components/order/OrderHeader.vue'
 import { useOrderStore } from '../stores/order.store';
 import { storeToRefs } from 'pinia';
 import SupplierCard from '../components/supplier/SupplierCard.vue';
+import AddressCard from '../components/addresses/AddressCard.vue';
+import AppAccordion from '../components/ui/AppAccordion.vue';
 const { orderData } = storeToRefs(useOrderStore())
 const { getOrderById } = useOrderStore();
 
