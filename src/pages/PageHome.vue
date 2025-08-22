0bd725a24be0d2af1455ddce3c29aade945a6534
Bruno Barross <altamiro_bruno@yahoo.com.br>
2025-08-22 18:00:23 -0300

    feat: show badge skeleton contact address

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,10 +1,10 @@
 <template>
-  <main class="pt-0 pb-6 lg:py-6">
-
-    <OrderHeader :order-number="orderData.value?.header?.number" :order-serial-number="orderData.value?.header?.serial"
-      :order-buyer="orderData.value?.header?.buyer" :order-buyer-contact="orderData.value?.header?.contact"
-      :order-created-at="orderData.value?.header?.createdAt" :order-status="orderData.value?.header?.status"
-      :order-currency="orderData.value?.header?.currency" :order-price="orderData.value?.header?.price" />
+  <main class="pt-0 pb-6 lg:py-6 overflow-x-hidden">
+    <OrderHeader :loading="loading" :order-number="orderData.value?.header?.number"
+      :order-serial-number="orderData.value?.header?.serial" :order-buyer="orderData.value?.header?.buyer"
+      :order-buyer-contact="orderData.value?.header?.contact" :order-created-at="orderData.value?.header?.createdAt"
+      :order-status="orderData.value?.header?.status" :order-currency="orderData.value?.header?.currency"
+      :order-price="orderData.value?.header?.price" />
     <section class="mt-8">
       <div class="container mx-auto lg:px-0 px-4">
         <AppCardSkeleton v-if="loading" />
@@ -20,11 +20,10 @@
           <template #content>
             <div class="grid grid-cols-1 md:grid-cols-3 items-start gap-4 mt-4">
               <template v-if="loading">
-                <AppCardSkeleton width-contact="100px" />
+                <AppCardSkeleton width-contact="100px" :show-badge="true" />
                 <AppCardSkeleton width-contact="100px" :two-columns="false" />
                 <AppCardSkeleton width-contact="100px" />
               </template>
-
               <template v-else-if="!loading && orderData.value?.addresses">
                 <AddressCard v-for="address in orderData.value.addresses" :key="address.id" :address="address" />
               </template>
