cfdbec338fff72ae4c1dd721b8215a3e1bc5ecf8
Bruno Barross <altamiro_bruno@yahoo.com.br>
2025-08-22 12:02:34 -0300

    feat:  configure a proxy on vite for bypass cors server api

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  console.log(env.VITE_BACKEND_URL);
+  return {
+    plugins: [vue(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    }
+  }
 })
