d33f961c61b8fce44979b816fbca35277173c29d
Bruno Barross <altamiro_bruno@yahoo.com.br>
2025-08-22 18:05:43 -0300

    style: change page title to "Order Details"

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,18 @@ import PageHome from '../pages/PageHome.vue'
 
 
 const routes = [
-    { path: '/', name: 'home', component: PageHome },
+    { path: '/', name: 'home', component: PageHome, meta: { title: 'Order Details' } },
 
 ]
 
 const router = createRouter({
     history: createMemoryHistory(),
     routes,
+})
+
+
+router.afterEach((to) => {
+  document.title = `Mercado Eletronico | ${to.meta.title}`
 })
 
 export default router;
