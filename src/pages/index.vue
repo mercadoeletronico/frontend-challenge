d024b1c6102f92bffdc18a9448b18c3846f875f9
Jeferson Santos de souza <jeferson.santos@jointecnologia.com.br>
2025-08-23 19:52:33 -0300

    add new component for render supplier info

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,15 @@
 <template>
   <CardPreOrder v-if="headerData" :header="headerData.header" />
+  <CardSupplierOrder v-if="headerData" :supplier="headerData.supplier" />
   <div v-else>Carregando...</div>
+
+
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import CardPreOrder from '@/components/cards/PreOrder.vue'
+import CardSupplierOrder from '@/components/cards/SupplierOrder.vue'
 import { usePreOrder } from '@/composables/api/usePreOrder'
 
 defineOptions({
