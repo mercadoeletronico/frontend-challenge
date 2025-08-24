9015a11d0f5fc4df3841865365b51ccded684664
Jeferson Santos de souza <jeferson.santos@jointecnologia.com.br>
2025-08-23 22:22:42 -0300

    Ultimo commit - nota -> foi usado a versão free do font-awesome então alguns ícones estão fora do padrão de "regular"

--- cypress/e2e/responsive.cy.js
+++ cypress/e2e/responsive.cy.js
@@ -8,7 +8,6 @@ describe('Responsive Design', () => {
     cy.visit('/')
     cy.wait('@getPreOrder')
 
-    // Verifica se o conteúdo é visível em mobile
     cy.contains('Pre-Order').should('be.visible')
     cy.contains('MTP West Buyer').should('be.visible')
     cy.contains('Motion Industries INC.').should('be.visible')
@@ -19,7 +18,6 @@ describe('Responsive Design', () => {
     cy.visit('/')
     cy.wait('@getPreOrder')
 
-    // Verifica layout em tablet
     cy.contains('Addresses').should('be.visible')
     cy.get('.grid').should('be.visible')
   })
@@ -29,7 +27,6 @@ describe('Responsive Design', () => {
     cy.visit('/')
     cy.wait('@getPreOrder')
 
-    // Verifica layout em desktop
     cy.contains('Pre-Order').should('be.visible')
     cy.get('.lg\\:h-\\[110px\\]').should('be.visible')
   })
@@ -38,11 +35,9 @@ describe('Responsive Design', () => {
     cy.visit('/')
     cy.wait('@getPreOrder')
 
-    // Mobile - 1 coluna
     cy.viewport('iphone-6')
     cy.get('.md\\:grid-cols-3').should('exist')
 
-    // Desktop - 3 colunas
     cy.viewport(1280, 720)
     cy.get('.md\\:grid-cols-3').should('exist')
   })
