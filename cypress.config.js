83f805f3542a87756f87d45d058093c210d1b0ad
Jeferson Santos de souza <jeferson.santos@jointecnologia.com.br>
2025-08-23 22:16:40 -0300

    set config for cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173',
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: true,
+    screenshotOnRunFailure: true,
   },
 })
